2a91516c4406d7ca05eb50c6b9af8349ec367f34
HousniAbdellatife <abdellatife.housni@gmail.com>
2021-11-14 21:47:14 +0100

    add require to minutes and hours for Time Class

--- exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/Time.scala
@@ -1,9 +1,17 @@
 package com.lightbend.training.scalatrain
 
 class Time (val hours: Int = 0, val minutes: Int = 0) {
-  // TODO : verify that hours within 0 and 23
-  // TODO : verify that minutes within 0 and 59
+  require(hours <= 23 && hours >= 0, "invalid hours")
+  require(minutes <= 59 && minutes >= 0, "invalid minutes")
   val asMinutes: Int = hours * 60 + minutes
   def minus(that: Time): Int = asMinutes - that.asMinutes
   def -(that: Time): Int = minus(that)
+}
+
+object Time {
+  def fromMinutes(minutes: Int):Time = {
+    val hours = minutes / 60
+    val min = minutes % 60
+    new Time(hours, min)
+  }
 }
