f93cb1190741bd6c8758a6ce708635e3a0350e6f
HousniAbdellatife <abdellatife.housni@gmail.com>
2021-11-14 19:27:13 +0100

    add default values to Time class params

--- exercises/src/main/scala/Time.scala
+++ exercises/src/main/scala/Time.scala
@@ -1,4 +1,4 @@
-class Time (val hours: Int, val minutes: Int) {
+class Time (val hours: Int = 0, val minutes: Int = 0) {
   // TODO : verify that hours within 0 and 23
   // TODO : verify that minutes within 0 and 59
   val asMinutes: Int = hours * 60 + minutes
