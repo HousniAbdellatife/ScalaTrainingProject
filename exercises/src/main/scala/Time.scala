ddfca24fa810acd71e639fa685f755adb186ff60
HousniAbdellatife <abdellatife.housni@gmail.com>
2021-11-14 18:58:26 +0100

    add minus method to Time class

--- exercises/src/main/scala/Time.scala
+++ exercises/src/main/scala/Time.scala
@@ -2,5 +2,5 @@ class Time (val hours: Int, val minutes: Int) {
   // TODO : verify that hours within 0 and 23
   // TODO : verify that minutes within 0 and 59
   val asMinutes: Int = hours * 60 + minutes;
-
+  def minus(that: Time): Int = asMinutes - that.asMinutes;
 }
