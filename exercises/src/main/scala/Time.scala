fdd8beccd56b05b849ac52066c994146724d72cb
HousniAbdellatife <abdellatife.housni@gmail.com>
2021-11-14 19:13:06 +0100

    add - alias method of minus method

--- exercises/src/main/scala/Time.scala
+++ exercises/src/main/scala/Time.scala
@@ -1,6 +1,7 @@
 class Time (val hours: Int, val minutes: Int) {
   // TODO : verify that hours within 0 and 23
   // TODO : verify that minutes within 0 and 59
-  val asMinutes: Int = hours * 60 + minutes;
-  def minus(that: Time): Int = asMinutes - that.asMinutes;
+  val asMinutes: Int = hours * 60 + minutes
+  def minus(that: Time): Int = asMinutes - that.asMinutes
+  def -(that: Time): Int = minus(that)
 }
