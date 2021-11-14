c8789449ab8e0bbd4948727b027984d860fc3a86
HousniAbdellatife <abdellatife.housni@gmail.com>
2021-11-14 16:58:22 +0100

    add asMinutes field to Time class

--- exercises/src/main/scala/Time.scala
+++ exercises/src/main/scala/Time.scala
@@ -1,5 +1,6 @@
 class Time (val hours: Int, val minutes: Int) {
   // TODO : verify that hours within 0 and 23
   // TODO : verify that minutes within 0 and 59
+  val asMinutes: Int = hours * 60 + minutes;
 
 }
