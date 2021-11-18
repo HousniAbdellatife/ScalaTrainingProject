c5d43be76cc26b27f1bd0784c0c63a1055f5b058
HousniAbdellatife <abdellatife.housni@gmail.com>
2021-11-18 13:23:00 +0100

    add Station class

--- exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -2,8 +2,7 @@ package com.lightbend.training.scalatrain
 
 import scala.collection.immutable.Seq;
 
-case class Train(number: Int, kind: String, schedule: Seq[Station]) {
-  {
+case class Train(number: Int, kind: String, schedule: Seq[(Time, Station)]) {
     require(schedule.size == 2, "Schedule must contains at least two elements")
-  }
+    val stations: Seq[Station] = schedule.map(_._2)
 }
